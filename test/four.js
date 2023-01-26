045ac67e22e964cd72c37acf4317d02ee93725ae
mudaseerahmed <hellohow491@gmail.com>
2023-01-26 19:35:11 +0530

    updated code four

--- test/four.js
+++ test/four.js
@@ -26,8 +26,35 @@ await $("#cboxClose").click();
 let na=await $("(//h3[@class='product-name'])[1]").getText();
 console.log(na+"is first");
 
-         
-         
-      })
+          })
+          it("get the text of the bulbs",async ()=>{
+   
+            let text=await $$("//h3/a");
+            for(let i=0;i<text.length;i++){
 
+                 console.log(await text[i].getText()+"is the price text");
+
+            }
+await browser.scroll(0,-400);
+await $(".list").click();
+              let text1=await $$("//h3/a");
+            for(let i=0;i<text1.length;i++){
+
+                 console.log(await text1[i].getText()+"is the price text for second one ");
+
+            }
+             await browser.scroll(0,400);
+             let na=await $(".title").getText();
+console.log(na+"is first in list view");
+               })
+
+
+               it("click on remove and validate the text",async ()=>{
+                 await $(".remove").click();
+                 let empty=await $(".list_empty").getText();
+                 console.log(empty+"  is after removing the text");
+                    
+                       
+                       
+                    })
     });
